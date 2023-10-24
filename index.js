47f6301749c3d9bf48990bb150a58b20ac5dc6d9
Zor-Rauq <zorrauq@gmail.com>
2023-10-24 10:15:59 +0200

    feat(timer): add game timer and handle game over

--- index.js
+++ index.js
@@ -127,6 +127,35 @@ function rectangularCollision(rectangle1, rectangle2) {
   );
 }
 
+let timer = 10;
+let timerId;
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = setTimeout(() => {
+      decreaseTimer();
+    }, 1000);
+    timer--;
+    document.querySelector("#timer").innerHTML = timer;
+  }
+  if (timer === 0) {
+    determineWinner({ player, ennemy, timerId });
+  }
+}
+
+decreaseTimer();
+
+function determineWinner({ player, ennemy, timerId }) {
+  clearTimeout(timerId);
+  document.querySelector("#displayText").style.display = "flex";
+  if (player.health === ennemy.health) {
+    document.querySelector("#displayText").innerHTML = "Tie !";
+  } else if (player.health > ennemy.health) {
+    document.querySelector("#displayText").innerHTML = "Player 1 win !";
+  } else if (player.health < ennemy.health) {
+    document.querySelector("#displayText").innerHTML = "Player 2 win !";
+  }
+}
+
 function animate() {
   window.requestAnimationFrame(animate);
 
@@ -165,6 +194,10 @@ function animate() {
     ennemy.isAttacking = false;
     player.health -= 20;
     document.querySelector("#playerHealth").style.width = player.health + "%";
+  }
+
+  if (ennemy.health <= 0 || player.health <= 0) {
+    determineWinner({ player, ennemy, timerId });
   }
 }
 
